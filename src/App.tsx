81db3b9d33ac606cef8619c9635880b5d768170b
sumiok <tap.miwo@gmail.com>
2023-10-22 16:38:10 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculater from './component/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculater />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Calculator.tsx
@@ -0,0 +1,6 @@
+export default function Calculater(){
+  return (
+    <div>
+    </div>
+  )
+}
